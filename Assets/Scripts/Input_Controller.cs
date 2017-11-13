f3abd779d454528327a1ed83ade36981267f5ac5
Samuel (Yomi) <yomi.akinmulero@gmail.com>
2017-11-12 23:19:02 -0800

    Added scripts making game more playable.

---
+++ Assets/Scripts/Input_Controller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_Controller : MonoBehaviour {
+
+    public GameObject leftPaddle;
+    public GameObject rightPaddle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //VEGETA LEFT PADDLE
+        //Sets velocity to 0
+        leftPaddle.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+
+        //Moves left paddle (Vegeta) with Q key
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //Sets velocity to move up 1
+            leftPaddle.GetComponent<Rigidbody>().velocity = new Vector3(0f, 5f, 0f);
+            //Debug.Log("Vegeta is ascending.");
+        }
+
+        //Moves left paddle (Vegeta) with A key
+       else if (Input.GetKey(KeyCode.A))
+        {
+            //Sets velocity to move down -1
+            leftPaddle.GetComponent<Rigidbody>().velocity = new Vector3(0f, -5f, 0f);
+
+            // Debug.Log("Vegeta is descending.");
+        }
+
+        //GOKU RIGHT PADDLE
+        //Sets velocity to 0
+        rightPaddle.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+
+        //Moves right paddle (Vegeta) with up key
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Sets velocity to move up 1
+            rightPaddle.GetComponent<Rigidbody>().velocity = new Vector3(0f, 5f, 0f);
+            //Debug.Log("Goku is ascending.");
+        }
+
+        //Moves right paddle (Goku) with down key
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Sets velocity to move down -1
+            rightPaddle.GetComponent<Rigidbody>().velocity = new Vector3(0f, -5f, 0f);
+
+            // Debug.Log("Goku is descending.");
+        }
+    }
+}
---
+++ Assets/Scripts/Ki_Ball_Controller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ki_Ball_Controller : MonoBehaviour {
+
+    //Reference to rigidbdy attached to Ki Ball
+    Rigidbody rigbod;
+
+	// Use this for initialization
+	void Start () {
+
+        rigbod = GetComponent<Rigidbody>();
+
+        //Random x direction
+        int xDir = Random.Range(0, 2);
+
+        //Random y direction
+        int yDir = Random.Range(0, 3);
+
+        Vector3 launchDir = new Vector3();
+
+        if (xDir == 0)
+        {
+            launchDir.x = -8f;
+        }
+
+        if (xDir == 1)
+        {
+            launchDir.x = 8f;
+        }
+
+        if (yDir == 0)
+        {
+            launchDir.y = -8f;
+        }
+
+        if (yDir == 1)
+        {
+            launchDir.y = 8f;
+        }
+
+        if (yDir == 2)
+        {
+            launchDir.y = 0f;
+        }
+        rigbod.velocity = launchDir;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision hit) {
+
+        Debug.Log(hit.gameObject.name);
+    }
+}
